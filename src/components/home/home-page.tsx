7853fe566ce585b0ed8d1b4cb37ef8a3477afcbc
BlueStarBurst <hargreaves.bryant@gmail.com>
2024-11-19 17:04:45 -0600

    react native is not very fun

---
+++ src/components/home/home-page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { TileContainer } from "../tiles/TileContainer";
+
+import mapsLogo from "@/assets/images/maps.png";
+
+export default function HomePage() {
+    return (
+        <>
+            <div className="absolute top-0 left-0 w-full h-full">
+                <TileContainer />
+            </div>
+            <div className="relative z-10">
+                <div className="p-5">
+                    <h1 className="text-6xl"><b>GeoGo</b></h1>
+                    
+                    <p className="text-2xl">Explore the world in 3D</p>
+                    <div className="flex flex-row items-center justify-start gap-1">
+                        <p>Built with </p>
+                        <Image src={mapsLogo} alt="Google Maps" className="max-h-5 object-contain w-max" />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
